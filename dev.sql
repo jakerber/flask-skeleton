8f557c785c5cadbbed77688c14153ad5152e175f
Joshua Kerber <jkerber14@comcast.net>
2021-05-12 01:05:10 -0700

    Remove phone #s

--- dev.sql
+++ dev.sql
@@ -10,9 +10,9 @@
 -- Sample user table entries
 INSERT INTO users (id, phone, name, password, created_on)
 VALUES
-    (1, 9784605401, 'josh', '1a0a41d838bbff454688436ff593508dab2c55722bae0f32d6ba50975a6a46a3', 'Wed, 12 May 2021 02:20:01 GMT'),
-    (2, 9784605212, 'joey', 'dc6a8cfa75129796c929f93a4dbe914604f71ea27f7d0ff6d8ed72d75226ecd7', 'Wed, 12 May 2021 02:20:02 GMT'),
-    (3, 9784605240, 'alec', 'efd851b3a9fc641a5316e0c8cff544c070448ffadf9772348b5391dd02ad2f89', 'Wed, 12 May 2021 02:20:03 GMT');
+    (1, 1111111111, 'josh', '1a0a41d838bbff454688436ff593508dab2c55722bae0f32d6ba50975a6a46a3', 'Wed, 12 May 2021 02:20:01 GMT'),
+    (2, 2222222222, 'joey', 'dc6a8cfa75129796c929f93a4dbe914604f71ea27f7d0ff6d8ed72d75226ecd7', 'Wed, 12 May 2021 02:20:02 GMT'),
+    (3, 3333333333, 'alec', 'efd851b3a9fc641a5316e0c8cff544c070448ffadf9772348b5391dd02ad2f89', 'Wed, 12 May 2021 02:20:03 GMT');
 
 -- Sample stuff table entries
 INSERT INTO stuff (description, user_id, created_on)
